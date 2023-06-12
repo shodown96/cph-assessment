4ce82415d640fb57cd301c17617deb6ccc4c79a1
Elijah Soladoye <shodown96@gmail.com>
2023-06-12 14:37:08 +0100

    refactor

--- dpk.js
+++ dpk.js
@@ -1,26 +1,18 @@
 const crypto = require("crypto");
 
 exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
+
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+  let candidate = "0";
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+  if (event && event.partitionKey) {
+    candidate = typeof candidate !== "string" ? JSON.stringify(event.partitionKey) : event.partitionKey
+  } else if(event) {
+    const data = JSON.stringify(event);
+    candidate = crypto.createHash("sha3-512").update(data).digest("hex");
   }
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
+
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
     candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
   }
